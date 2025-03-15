a34d2f4fe7ca3e8447a9e60b193e07372540b0d4
tsmotlp <yqhuang2912@gmail.com>
2025-03-15 15:23:26 +0800

    fix url

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
